8b60f4b894c6fd52aa7d63a00cdc74a9c112eed2
SeokYoung.Kim <crom@zzizily.com>
2018-12-18 03:58:41 +0900

    mybatis 이용한 mariadb, h2 연동

--- spring-boot-skeleton-web/src/main/resources/import.sql
+++ spring-boot-skeleton-web/src/main/resources/import.sql
@@ -17,8 +17,9 @@
 drop table if exists city;
 drop table if exists hotel;
 
-create table city (id int primary key auto_increment, name varchar, state varchar, country varchar);
-create table hotel (city int, name varchar, address varchar, zip varchar);
+create table city (id int primary key auto_increment, name varchar(255), state varchar(255), country varchar(255));
+create table hotel (city int, name varchar(255), address varchar(255), zip varchar(255));
 
 insert into city (name, state, country) values ('San Francisco', 'CA', 'US');
-insert into hotel(city, name, address, zip) values (1, 'Conrad Treasury Place', 'William & George Streets', '4001')
+insert into city (name, state, country) values ('서초구', 'SE', 'KR');
+insert into hotel(city, name, address, zip) values (1, 'Conrad Treasury Place', 'William & George Streets', '4001');
