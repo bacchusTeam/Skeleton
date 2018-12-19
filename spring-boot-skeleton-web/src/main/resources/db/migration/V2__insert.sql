309312d689d6b8167b2ab132769a134ea98190d5
SeokYoung.Kim <crom@zzizily.com>
2018-12-20 00:56:22 +0900

    flyway 및 jpa 사용 mybatis 차이 및 flyway 이용한 db migration

---
+++ spring-boot-skeleton-web/src/main/resources/db/migration/V2__insert.sql
@@ -0,0 +1,3 @@
+insert into PERSON (first_name, last_name) values ('미선', '김');
+insert into PERSON (first_name, last_name) values ('솔희', '이');
+insert into PERSON (first_name, last_name) values ('석영', '김');
