af6c027539e87728b5d01935feea80ff5b3e312f
SeokYoung.Kim <crom@zzizily.com>
2018-12-25 07:04:19 +0900

    secruity public url 추가

--- spring-boot-skeleton-web/src/main/java/sample/config/SecurityConfig.java
+++ spring-boot-skeleton-web/src/main/java/sample/config/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-          .antMatchers("/webjars/**", "/script/**", "/css/**").permitAll().anyRequest().authenticated()
+          .antMatchers("/", "/home", "/webjars/**", "/script/**", "/css/**").permitAll().anyRequest().authenticated()
             .and()
           .formLogin()
             .loginPage("/login")
