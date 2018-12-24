6aa8ba1c10a730cbbb4ff1396118605c17c223a6
SeokYoung.Kim <crom@zzizily.com>
2018-12-25 07:03:37 +0900

    webjars MVC 적용 시 오류발생 webjars 가이드 상에는 mvc 적용해야 한다고 나오지만 (https://www.webjars.org/documentation#springmvc) 실제 적용 하니깐 404 발생 spring boot 2.0 에따른 변화가 원인은 확인 안됨

--- spring-boot-skeleton-web/src/main/java/sample/config/MvcConfig.java
+++ spring-boot-skeleton-web/src/main/java/sample/config/MvcConfig.java
@@ -1,15 +1,25 @@
 package sample.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-  public void addViewControllers(ViewControllerRegistry registry) {
-    registry.addViewController("/hello").setViewName("hello");
-    registry.addViewController("/login").setViewName("login");
+  @Override
+  public void addViewControllers(ViewControllerRegistry viewControllerRegistry) {
+    viewControllerRegistry.addViewController("/hello").setViewName("hello");
+    viewControllerRegistry.addViewController("/login").setViewName("login");
   }
+
+  /**
+   * 404 발생에 따른 주석 처리
+   *   @Override
+   *   public void addResourceHandlers (ResourceHandlerRegistry resourceHandlerRegistry) {
+   *     resourceHandlerRegistry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+   *   }
+   */
 
 }
