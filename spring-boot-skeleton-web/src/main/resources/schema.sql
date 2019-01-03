47692a6e11d5164f00d0727df33dce4be26ff65f
SeokYoung.Kim <crom@zzizily.com>
2019-01-03 21:28:10 +0900

    datasource 설정 추가 data.sql 과 schema.sql 분리

--- spring-boot-skeleton-web/src/main/resources/schema.sql
+++ spring-boot-skeleton-web/src/main/resources/schema.sql
@@ -19,7 +19,3 @@ drop table if exists hotel;
 
 create table city (id int primary key auto_increment, name varchar(255), state varchar(255), country varchar(255));
 create table hotel (id int primary key auto_increment,city int, name varchar(255), address varchar(255), zip varchar(255));
-
-insert into city (name, state, country) values ('San Francisco', 'CA', 'US');
-insert into city (name, state, country) values ('서초구', 'SE', 'KR');
-insert into hotel(city, name, address, zip) values (1, 'Conrad Treasury Place', 'William & George Streets', '4001');
